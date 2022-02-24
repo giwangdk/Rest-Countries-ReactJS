244caadb7005368777f221d6c77af5ffc622b487
gidwikintan <gidwikintan@gmail.com>
2022-02-24 23:20:58 +0700

    add eslint

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 function Button({label, onClick }) {
   return (
-    <button className="px-2 py-2 border-gray-700" onClick={onClick}>{label}</button>
+    <button className="px-2 py-2 border-gray-700 text-gray-700" onClick={onClick}>{label}</button>
   )
 }
 
