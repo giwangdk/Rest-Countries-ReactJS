949b3503b06696b668e5b42cb67a09dd16fb327b
gidwikintan <gidwikintan@gmail.com>
2022-02-24 23:38:22 +0700

    setup eslint

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Button({label, onClick }) {
+function Button({ label, onClick }) {
   return (
-    <button className="px-2 py-2 border-gray-700 text-gray-700" onClick={onClick}>{label}</button>
-  )
+    <button
+      className="px-2 py-2 border-gray-700 text-gray-700"
+      onClick={onClick}
+    >
+      {label}
+
+    </button>
+  );
 }
 
 Button.propTypes = {
-    label: PropTypes.string,
-    onClick : PropTypes.func
-}
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+};
 
 Button.defaultProps = {
-    label : 'Button',
-    onClick : () => {}
-}
+  label: 'Button',
+  onClick: () => {},
+};
 
-export default Button
+export default Button;
