e410dacc9740c4d494ee1d1da27805485b09a5fd
gidwikintan <gidwikintan@gmail.com>
2022-02-24 23:06:50 +0700

    layouting button component, setup font

---
+++ src/components/atoms/Button/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Button({label, onClick }) {
+  return (
+    <button className="px-2 py-2 border-gray-700" onClick={onClick}>{label}</button>
+  )
+}
+
+Button.propTypes = {
+    label: PropTypes.string,
+    onClick : PropTypes.func
+}
+
+Button.defaultProps = {
+    label : 'Button',
+    onClick : () => {}
+}
+
+export default Button
