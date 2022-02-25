109c4827bcf08f4ff8aa6ed5e2f43e9352ada84a
gidwikintan <gidwikintan@gmail.com>
2022-02-25 14:17:24 +0700

    install fontawesome

--- src/components/atoms/Search/index.jsx
+++ src/components/atoms/Search/index.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable quotes */
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Search() {
   return (
     <div>
       <form>
+        <FontAwesomeIcon icon={["fab", "microsoft"]} />
         <input type="text" name="" id="" />
       </form>
     </div>
