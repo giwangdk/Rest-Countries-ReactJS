3a65bead10118bdb8841b59f31297d17d32e4d5d
gidwikintan <gidwikintan@gmail.com>
2022-02-25 14:15:00 +0700

    layouting search component

---
+++ src/components/atoms/Search/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+import React from "react";
+
+function Search() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="" id="" />
+      </form>
+    </div>
+  );
+}
+
+export default Search;
