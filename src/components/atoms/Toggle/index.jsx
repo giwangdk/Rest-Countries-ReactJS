1562bcffe1da734f14b088c06ed31e02e88fefea
gidwikintan <gidwikintan@gmail.com>
2022-02-25 20:43:51 +0700

    layouting navbar

---
+++ src/components/atoms/Toggle/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable quotes */
+import React from "react";
+
+function Toggle() {
+  return (
+    <div>
+      <p>Dark Mode</p>
+    </div>
+  );
+}
+
+export default Toggle;
---
+++ src/components/molecules/Navbar/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="nav-brand">
+        <h5>Where in the world?</h5>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
