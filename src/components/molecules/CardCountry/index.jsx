8cbb99634b778060ddc05fa29e9ecc12a308af8e
gidwikintan <gidwikintan@gmail.com>
2022-03-02 16:54:56 +0700

    integrate to card country

--- src/components/molecules/CardCountry/index.jsx
+++ src/components/molecules/CardCountry/index.jsx
@@ -1,27 +1,28 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable quotes */
 import React from "react";
 
-function CardCountry() {
+function CardCountry({ country }) {
   return (
     <div className="card border shadow shadow-gray-300">
       <div className="card-img">
-        <img src="../../../logo.svg" alt="" />
+        <img src={country?.flags?.png} alt="" />
       </div>
       <div className="card-body text-left p-3 ">
         <div className="card-title mb-3">
-          <h2 className="font-bold text-lg">Australia</h2>
+          <h2 className="font-bold text-lg">{country?.name?.official}</h2>
         </div>
         <div className="card-desc">
           <ul>
             <li>
-              <b>Population :</b> 234566
+              <b>Population :</b> {country?.population}
             </li>
             <li>
-              <b>Region :</b> Oceania
+              <b>Region :</b> {country?.region}
             </li>
             <li>
-              <b>Capital :</b> Oceania
+              <b>Capital :</b> {country?.capital?.[0]}
             </li>
           </ul>
         </div>
