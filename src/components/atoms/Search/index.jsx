d3bab15ca9520f7bd32bcef203e368f1eda52f61
gidwikintan <gidwikintan@gmail.com>
2022-03-02 17:14:33 +0700

    integrate function submit and handlechange

--- src/components/atoms/Search/index.jsx
+++ src/components/atoms/Search/index.jsx
@@ -1,19 +1,34 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useState } from "react";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Search() {
+function Search({ action }) {
+  const [value, setValue] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    action(value);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+
   return (
     <div className="bg-white shadow shadow-gray-500  w-80 rounded px-3 py-1 flex justify-between items-center">
       <FontAwesomeIcon icon={faSearch} color="gray" />
-      <input
-        type="text"
-        name=""
-        id=""
-        className="  outline-gray-100 py-1 px-2 w-full ml-3"
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={handleChange}
+          className="  outline-gray-100 py-1 px-2 w-full ml-3"
+        />
+      </form>
     </div>
   );
 }
