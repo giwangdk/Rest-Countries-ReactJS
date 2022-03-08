0d367e9445f97b034b2b76f671a9531252a13789
gidwikintan <gidwikintan@gmail.com>
2022-03-08 21:07:46 +0700

    feat(API): fetch api by code
    - Use fetch data function for fetching data by code in detail page

--- src/Pages/Detail/index.jsx
+++ src/Pages/Detail/index.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../components/atoms/Button";
 import BorderCountries from "../../components/molecules/BorderCountries";
 import DescriptionBody from "../../components/molecules/DescriptionBody";
+import { fetchData } from "../../utils/fetchData";
 
 function Detail() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData(setLoading, setError, setData, `alpha/per`);
+  }, []);
+
+  console.log("detail", data);
+
   return (
     <>
       <Button label="Back" />
